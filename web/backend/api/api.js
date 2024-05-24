725f48ff558b40615438aa30fe73277ff8fe5cc2
Rani Zouaoui <88784948+ranizouaoui@users.noreply.github.com>
2024-05-24 15:35:41 +0100

    Update api.js

--- web/backend/api/api.js
+++ web/backend/api/api.js
@@ -1,8 +1,8 @@
 // api.js
 module.exports.mqtt_credentials = {
-    host: 'c05d8790191940fe9879853741b02cd4.s2.eu.hivemq.cloud',
+    host: 'xxxxxxxxxxxxxxxx.s2.eu.hivemq.cloud',
     port: 8883,
     protocol: 'mqtts',
-    username: 'greenhause2023',
-    password: 'greenhause2023'
+    username: 'xxxxxxxxxxxxxx',
+    password: 'xxxxxxxxxxxxxxxx'
 };
